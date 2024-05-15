e4e92c2cfef6bdf7b74a3d17966ac1d5bcc432e5
ItishaBhatiya <itishabhatiya7@gmail.com>
2024-05-15 18:16:46 +0530

    signup

---
+++ component/footer.js
@@ -0,0 +1,84 @@
+const footer = () => {
+    return `<hr class="border border-secondary border-1 opacity-20">
+    <footer class="padding-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="footer-box2">
+                    <h2>About Us</h2>
+                    <ul>
+                        <li>&nbsp About us </li>
+                        <li>&nbsp Our Product</li>
+                        <li>&nbsp Collection</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="footer-box2">
+                    <h2>Contact</h2>
+                    <ul>
+                        <li><i class="fa-solid fa-location-dot"></i> &nbsp 184 Main Collins Street</li>
+                        <li><i class="fa-solid fa-phone"></i> &nbsp(226) 446 9371</li>
+                        <li><i class="fa-solid fa-envelope"></i> &nbsp ring@gmail.com</li>
+                        <li><i class="fa-solid fa-earth-asia"></i> &nbsp www.google.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="footer-box3">
+                    <h2>Workshops</h2>
+                    <ul>
+                        <li>OSHA Compliance</li>
+                        <li>Microsoft Excel Basics</li>
+                        <li>Forum Speaker Series</li>
+                        <li>Tedx Moscow Conference</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="footer-box4 gallery">
+                    <h2>Gallery</h2>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/1.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/2.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/3.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/4.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/5.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-img-box">
+                                <img src="/images/6.jpg.webp" alt="error" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="border-bottom"></div>
+        <div class="row copyright">
+            <p>Copyright ©2024 All rights reserved | This Project is made with <i class="fa-regular fa-heart"></i> by ITISHA BHATIYA</p>
+        </div>
+        </div>
+</footer>`
+}
+export default footer
+
